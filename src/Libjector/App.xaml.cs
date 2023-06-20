cb6a589393e347bbffcf7498e5985dd2df5b49ed
Jistx <116097008+Jistx@users.noreply.github.com>
2023-06-20 11:22:39 -0400

    update file

---
+++ src/Libjector/App.xaml.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+using System.Windows.Threading;
+using Libjector.Core;
+
+namespace Libjector;
+
+public partial class App
+{
+    public static Settings Settings { get; } = Settings.Load();
+
+    private void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+    {
+        MessageBox.Show("An unhandled exception occurred! " + args.Exception.Message, "Libjector");
+        args.Handled = true;
+    }
+
+    private void OnExit(object sender, ExitEventArgs args)
+    {
+        Settings.Save();
+    }
+}
